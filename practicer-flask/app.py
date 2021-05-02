370c7c0eb9855338fe415b9909767ddcf70511af
DominikPott <d_tronic_p@gmx.de>
2021-05-02 14:21:57 +0200

    added /.idea to gitignore updated requirements to inlcude practicer from github

--- practicer-flask/app.py
+++ practicer-flask/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
+from practicer import api
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello World."
+    e = [e.get('label', "NoName") for e in api.exercises()]
+    return "Exercises: " + "| ".join(e)
 
 
 if __name__ == "__main__":
