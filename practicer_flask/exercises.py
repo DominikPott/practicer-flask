3e3b87788186eb89331f9be2f34ec1d5821cb0fe
DominikPott <d_tronic_p@gmx.de>
2021-05-05 21:49:29 +0200

    added categories for the exercises

--- practicer_flask/exercises.py
+++ practicer_flask/exercises.py
@@ -10,13 +10,14 @@ bp = Blueprint('exercises', __name__, )
 def exercise():
     import os
     exercises = practicer_flask.exercise_inventory.exercises()
+    categories = {}
     for exercise in exercises:
         path = exercise['path']
         directory = os.path.dirname(path)
-
         exercise['thumbnail'] = 'exercises/' + os.path.basename(os.path.dirname(directory)) + '/' + os.path.basename(
             directory) + '/' + exercise['thumbnail']
         exercise['template'] = 'exercises/' + os.path.basename(os.path.dirname(directory)) + '/' + os.path.basename(
             directory) + '/' + exercise['template']
-        log.debug(exercise['thumbnail'])
-    return render_template("exercises.html", exercises=exercises)
+        categories.setdefault(exercise.get('categories', ['no categorie'])[0], []).append(exercise)
+
+    return render_template("exercises.html", categories=categories)
