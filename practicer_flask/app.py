d2d5e4fefc51c899124e8ad6e872eadf536117e1
DominikPott <d_tronic_p@gmx.de>
2021-05-12 18:35:08 +0200

    added float layout

--- practicer_flask/app.py
+++ practicer_flask/app.py
@@ -1,11 +1,10 @@
-from flask import Flask, url_for
+from flask import Flask
 
 import practicer_flask.exercises
-from practicer_flask import log
 
 app = Flask(__name__)
 
 app.register_blueprint(practicer_flask.exercises.bp)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
