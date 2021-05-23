57c789c0f2e9accb8ef2b638b7c265f582368a7c
DominikPott <d_tronic_p@gmx.de>
2021-05-23 23:15:48 +0200

    removed models from header

--- practicer_flask/app.py
+++ practicer_flask/app.py
@@ -8,14 +8,17 @@ import practicer_flask.dashboard
 import practicer_flask.topic
 import practicer_flask.model_viewer
 
-app = Flask(__name__)
 
-app.register_blueprint(practicer_flask.auth.bp)
-app.register_blueprint(practicer_flask.exercises.bp)
-app.register_blueprint(practicer_flask.dashboard.bp)
-app.register_blueprint(practicer_flask.topic.bp)
-app.register_blueprint(practicer_flask.model_viewer.bp)
+def create_app(test_config=None):
+    app = Flask(__name__)
 
+    app.register_blueprint(practicer_flask.auth.bp)
+    app.register_blueprint(practicer_flask.exercises.bp)
+    app.register_blueprint(practicer_flask.dashboard.bp)
+    app.register_blueprint(practicer_flask.topic.bp)
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=os.environ.get("DEV", False))
