3d46d2dfa8297b6c488fba7a616aa8e1f5397f8b
DominikPott <d_tronic_p@gmx.de>
2021-05-30 17:43:09 +0200

    added required login for dashboard added forum placeholder

--- practicer_flask/dashboard.py
+++ practicer_flask/dashboard.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 
+from practicer_flask.auth import login_required
 import practicer_flask.exercise_inventory
 import practicer_flask.user_exercise_stats.api as statistics_api
 
@@ -8,18 +9,20 @@ bp = Blueprint("dashboard", __name__)
 
 @bp.route("/dashboard")
 @bp.route("/dashboard/<exercise_uuid>")
+@login_required
 def dashboard(exercise_uuid=None):
+    user = g.user['id']
     if exercise_uuid:
-        statistics_api.increase_experience(user=0, exercise=exercise_uuid)
-        statistics_api.increase_streak(user=0)
+        statistics_api.increase_experience(user=user, exercise=exercise_uuid)
+        statistics_api.increase_streak(user=user)
         exercise_ = practicer_flask.exercise_inventory.exercise_from_uuid(exercise_uuid)
-        statistics_api.add_exercise_to_history(user=0, exercise=exercise_)
+        statistics_api.add_exercise_to_history(user=user, exercise=exercise_)
 
-    streak = statistics_api.streak(user=0)
-    history = statistics_api.history(user=0)
+    streak = statistics_api.streak(user=user)
+    history = statistics_api.history(user=user)
     exercises = practicer_flask.exercise_inventory.exercises()
     exercises_history = _map_exercises_to_history(history, exercises)
-    experiences = statistics_api.experience(user=0)
+    experiences = statistics_api.experience(user=user)
     exercises = enrich_exercises_with_experiences(exercises, experiences)
     return render_template("dashboard.html", exercises=exercises, exercises_history=exercises_history, streak=streak)
 
